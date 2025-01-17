3238fa3de78e5dd9267d21015899c28700b441cb
Kirill Tregubov <contact@kirilltregubov.com>
2025-01-17 02:08:21 -0500

    add second repro

--- app.vue
+++ app.vue
@@ -3,6 +3,7 @@
     <nav>
       <NuxtLink to="/">One child is textContent</NuxtLink>
       <NuxtLink to="/broken">Both children are elements (broken)</NuxtLink>
+      <NuxtLink to="/different-broken">Both children are elements (broken)</NuxtLink>
       <NuxtLink to="/fixed">Different parent elements (fixed)</NuxtLink>
     </nav>
     <NuxtPage />
---
+++ pages/different-broken.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h4>This is also broken ONLY on a fresh prod load (reload the page to see).</h4>
+    <h4>Both fallback and client children are elements, and the client child is enclosed in a Transition element in ClientOnly:</h4>
+    <ClientOnly>
+      <div>
+        <Transition>
+          <div>client</div>
+        </Transition>
+      </div>
+
+      <template #fallback>
+        <div>
+          <div>fallback</div>
+        </div>
+      </template>
+    </ClientOnly>
+  </div>
+</template>
